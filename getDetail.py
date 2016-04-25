a67b989b764dcd4d6dd37b8f1f3848cf7b660163
tcx1000 <tcx1000@gmail.com>
2016-04-25 15:10:14 +0800

    Detail of douban articles
    detail data of douban topic group articles

---
+++ getDetail.py
@@ -0,0 +1,37 @@
+#coding=utf-8
+import urllib
+import re
+import sys
+
+
+def getHtml(url):
+    
+    page = urllib.urlopen(url)
+    html = page.read()
+
+    return html
+
+
+def getInfo(html,location):
+    
+    pub_info = re.findall(r'<a href=".*" title=".*'+location+'.*" class="">',html)
+    pub_date = re.findall(r'<td nowrap="nowrap" class="time">.*</td>',html)
+    
+    count=0
+    for info in pub_info:
+    	new_info = info + pub_date[count]
+    	pub_info[count] = new_info
+        count=count+1
+
+    return pub_info  
+    
+
+def main():
+
+    html = getHtml('https://www.douban.com/group/topic/85541201/')
+
+    print html
+
+
+if __name__ == '__main__':
+    main()
